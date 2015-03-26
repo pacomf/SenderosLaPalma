b2c0e4ee002a0a5da1e8b796f6a3d564ebfba0c8
Jorge Carballo <jelcaf@gmail.com>
2015-03-26 13:23:51 +0000

    Issue #3: Guardamos la información de las respuestas seleccionadas en el cuestionario y las establecemos.

--- android/PateaLaPalma/app/src/main/java/com/jelcaf/pacomf/patealapalma/binding/QuestionRecommenderForm.java
+++ android/PateaLaPalma/app/src/main/java/com/jelcaf/pacomf/patealapalma/binding/QuestionRecommenderForm.java
@@ -12,6 +12,7 @@ public class QuestionRecommenderForm implements Serializable {
    private String question;
    private List<RecommenderQuestionResponse> posibleResponses;
    private ResponseType type;
+   private Integer selectedResponse;
 
    public QuestionRecommenderForm (String question, List<RecommenderQuestionResponse> posibleResponses, ResponseType type) {
       this.question = question;
@@ -30,5 +31,13 @@ public class QuestionRecommenderForm implements Serializable {
 
    public List<RecommenderQuestionResponse> getPosibleResponses() {
       return posibleResponses;
+   }
+
+   public Integer getSelectedResponse() {
+      return selectedResponse;
+   }
+
+   public void setSelectedResponse(Integer selectedResponse) {
+      this.selectedResponse = selectedResponse;
    }
 }
--- android/PateaLaPalma/app/src/main/java/com/jelcaf/pacomf/patealapalma/fragment/QuestionFragment.java
+++ android/PateaLaPalma/app/src/main/java/com/jelcaf/pacomf/patealapalma/fragment/QuestionFragment.java
@@ -3,9 +3,11 @@ package com.jelcaf.pacomf.patealapalma.fragment;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.view.ViewPager;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.TextView;
@@ -21,9 +23,11 @@ import com.jelcaf.pacomf.patealapalma.binding.RecommenderQuestionResponse;
 public class QuestionFragment extends Fragment {
 
    private static final String ARG_QUESTION = "question";
+   private static final String TAG = QuestionFragment.class.getCanonicalName();
 
    private ViewGroup mContainer;
    private QuestionRecommenderForm mQuestion;
+   private QuestionFragment mFragment;
 
    public static QuestionFragment newInstance(QuestionRecommenderForm question) {
       QuestionFragment fragment = new QuestionFragment();
@@ -55,18 +59,31 @@ public class QuestionFragment extends Fragment {
       RadioGroup mRadioGroup = (RadioGroup)v.findViewById(R.id.answer);
 
       for (int i=0; i < mQuestion.getPosibleResponses().size(); i++) {
-         createRadioButton(mRadioGroup, mQuestion.getPosibleResponses().get(i));
+         createRadioButton(mRadioGroup, mQuestion, i);
       }
+
+      StringBuilder stringBuilder = new StringBuilder();
+      stringBuilder.append(mQuestion.getQuestion() + " -> (" + mQuestion.getSelectedResponse() + ")");
+      if (mQuestion.getSelectedResponse() != null) {
+         stringBuilder.append(" " + ((RadioButton)mRadioGroup.getChildAt(mQuestion
+               .getSelectedResponse()))
+               .getText());
+         ((RadioButton) mRadioGroup.getChildAt(mQuestion.getSelectedResponse())).setChecked(true);
+      }
+      Log.i(TAG, stringBuilder.toString());
 
       return v;
    }
 
-   private void createRadioButton(RadioGroup mRadioGroup, RecommenderQuestionResponse response) {
+   private void createRadioButton(RadioGroup mRadioGroup, final QuestionRecommenderForm form, final int i) {
+      RecommenderQuestionResponse response = form.getPosibleResponses().get(i);
       RadioButton option = new RadioButton(getActivity());
       option.setText(response.text);
+      option.setLayoutParams(new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT));
       option.setOnClickListener(new View.OnClickListener() {
          @Override
          public void onClick(View v) {
+            form.setSelectedResponse(i);
             // TODO: Descomentar para pasar de pregunta cuando seleccionemos una respuesta
             // ((ViewPager)mContainer).setCurrentItem(((ViewPager)mContainer).getCurrentItem() + 1, true);
          }
