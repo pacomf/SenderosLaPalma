549cff306694baafb1b86d939ca3ba3a3dcd8cdc
Jorge Carballo <jelcaf@gmail.com>
2015-04-05 15:43:30 +0100

    Issue #3: Visualiza el menú flotante. Falta ver qué iconos llevarán cada una de las opciones de este menú.

--- android/PateaLaPalma/app/src/main/java/com/jelcaf/pacomf/patealapalma/activity/SenderoDetailWithImageActivity.java
+++ android/PateaLaPalma/app/src/main/java/com/jelcaf/pacomf/patealapalma/activity/SenderoDetailWithImageActivity.java
@@ -11,9 +11,10 @@ import android.support.v4.app.NavUtils;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.ViewTreeObserver;
 import android.widget.FrameLayout;
+import android.widget.ImageView;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import com.activeandroid.ActiveAndroid;
 import com.github.ksoichiro.android.observablescrollview.ObservableScrollView;
@@ -24,16 +25,20 @@ import com.jelcaf.pacomf.patealapalma.R;
 import com.jelcaf.pacomf.patealapalma.binding.dao.Sendero;
 import com.jelcaf.pacomf.patealapalma.fragment.SenderoDetailFragment;
 import com.jelcaf.pacomf.patealapalma.images.Utilities;
+import com.jelcaf.pacomf.patealapalma.login.LoginMethods;
+import com.jelcaf.pacomf.patealapalma.views.CustomDialogRating;
 import com.jelcaf.pacomf.patealapalma.views.CustomPopUpComments;
 import com.nineoldandroids.animation.Animator;
 import com.nineoldandroids.view.ViewHelper;
 import com.nineoldandroids.view.ViewPropertyAnimator;
+import com.oguzdev.circularfloatingactionmenu.library.FloatingActionMenu;
+import com.oguzdev.circularfloatingactionmenu.library.SubActionButton;
 
 /**
  * @author Jorge Carballo
  *         18/03/15
  */
-public class SenderoDetailWithImageActivity extends BaseActivity implements ObservableScrollViewCallbacks {
+public class SenderoDetailWithImageActivity extends BaseActivity implements ObservableScrollViewCallbacks, ViewTreeObserver.OnScrollChangedListener {
 
    private static final float MAX_TEXT_SCALE_DELTA = 0.3f;
    private static final boolean TOOLBAR_IS_STICKY = true;
@@ -44,12 +49,14 @@ public class SenderoDetailWithImageActivity extends BaseActivity implements Obse
    public static ObservableScrollView mScrollView;
    private TextView mTitleView;
    private View mFab;
+//   private ImageView mFab;
    private int mActionBarSize;
    private int mFlexibleSpaceShowFabOffset;
    private int mFlexibleSpaceImageHeight;
    private int mFabMargin;
    private int mToolbarColor;
    private boolean mFabIsShown;
+   private FloatingActionMenu mFloatMenu;
 
    private Sendero mSendero;
 
@@ -76,6 +83,9 @@ public class SenderoDetailWithImageActivity extends BaseActivity implements Obse
       mActionBarSize = getActionBarSize();
       mToolbarColor = getResources().getColor(R.color.primary);
 
+//      mFab = new ImageView(this); // Create an icon
+//      mFab.setImageDrawable(getResources().getDrawable(android.R.drawable.ic_menu_add));
+
       // Show the Up button in the action bar.
       getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
@@ -87,28 +97,29 @@ public class SenderoDetailWithImageActivity extends BaseActivity implements Obse
       mOverlayView = findViewById(R.id.overlay);
       mScrollView = (ObservableScrollView) findViewById(R.id.scroll);
       mScrollView.setScrollViewCallbacks(this);
+      mScrollView.getViewTreeObserver().addOnScrollChangedListener(this);
       mTitleView = (TextView) findViewById(R.id.title);
-//      mTitleView.setText(getTitle());
       mTitleView.setText(mSendero.getName());
       setTitle(null);
       final Activity activity = this;
       mFab = findViewById(R.id.fab);
-      mFab.setOnClickListener(new View.OnClickListener() {
-         @Override
-         public void onClick(View v) {
-            Toast.makeText(SenderoDetailWithImageActivity.this, "FAB is clicked", Toast.LENGTH_SHORT).show();
-            // TODO: En las opciones de subida de imagenes, rating etc, comprobar que este logueado (metodo de comprobacion creado en LoginMethods)
-            //if (LoginMethods.checkLogin(activity))
-            //   Utilities.selectImage(activity);
-            // TODO: Asignar la siguiente linea al VER TODOS LOS COMENTARIOS
-            // CustomPopUpComments.newInstance().show(getFragmentManager(), null);
-            // TODO: Asignar la siguiente linea al botón de RATING
-            //CustomDialogRating.showDialog(activity, idSendero, ratingActual);
-            // TODO: Asignar al "Como llegar" recuperando la Location del Sendero (geoStart)
-            Location senderoLocation = getCurrentLocation();
-            com.jelcaf.pacomf.patealapalma.network.Utilities.howToGoToSendero(activity, getCurrentLocation(), senderoLocation);
-         }
-      });
+
+//      mFab.setOnClickListener(new View.OnClickListener() {
+//         @Override
+//         public void onClick(View v) {
+//            // TODO: En las opciones de subida de imagenes, rating etc, comprobar que este logueado (metodo de comprobacion creado en LoginMethods)
+//            //if (LoginMethods.checkLogin(activity))
+//            //   Utilities.selectImage(activity);
+//            // TODO: Asignar la siguiente linea al VER TODOS LOS COMENTARIOS
+//            // CustomPopUpComments.newInstance().show(getFragmentManager(), null);
+//            // TODO: Asignar la siguiente linea al botón de RATING
+//            //CustomDialogRating.showDialog(activity, idSendero, ratingActual);
+//            // TODO: Asignar al "Como llegar" recuperando la Location del Sendero (geoStart)
+//            Location senderoLocation = getCurrentLocation();
+//            com.jelcaf.pacomf.patealapalma.network.Utilities.howToGoToSendero(activity, getCurrentLocation(), senderoLocation);
+//         }
+//      });
+
       mFabMargin = getResources().getDimensionPixelSize(R.dimen.margin_standard);
       ViewHelper.setScaleX(mFab, 0);
       ViewHelper.setScaleY(mFab, 0);
@@ -144,6 +155,60 @@ public class SenderoDetailWithImageActivity extends BaseActivity implements Obse
                .add(R.id.sendero_detail_container, fragment)
                .commit();
       }
+
+      buildFloatingMenu(activity, mSendero.getId().toString());
+   }
+
+   private void buildFloatingMenu(final Activity activity, final String idSendero) {
+      SubActionButton.Builder itemBuilder = new SubActionButton.Builder(this);
+
+      // Añadir imagen
+      ImageView itemIcon = new ImageView(this);
+      itemIcon.setImageDrawable(getResources().getDrawable(android.R.drawable.ic_media_play));
+      SubActionButton addImageButton = itemBuilder.setContentView(itemIcon).build();
+      addImageButton.setOnClickListener(new View.OnClickListener() {
+         @Override
+         public void onClick(View v) {
+            if (LoginMethods.checkLogin(activity)) {
+               Utilities.selectImage(activity);
+            }
+         }
+      });
+
+      // Añadir Comentario
+      itemIcon = new ImageView(this);
+      itemIcon.setImageDrawable(getResources().getDrawable(android.R.drawable.ic_menu_report_image));
+      SubActionButton addCommentButton = itemBuilder.setContentView(itemIcon).build();
+      addCommentButton.setOnClickListener(new View.OnClickListener() {
+         @Override
+         public void onClick(View v) {
+            CustomPopUpComments.newInstance().show(getFragmentManager(), null);
+         }
+      });
+
+      // Añadir Valoración
+      itemIcon = new ImageView(this);
+      itemIcon.setImageDrawable(getResources().getDrawable(android.R.drawable.ic_menu_camera));
+      SubActionButton addRatingButton = itemBuilder.setContentView(itemIcon).build();
+      addRatingButton.setOnClickListener(new View.OnClickListener() {
+         @Override
+         public void onClick(View v) {
+            CustomDialogRating.showDialog(activity, idSendero, 7);
+         }
+      });
+
+      // Qué ropa llevar
+      itemIcon = new ImageView(this);
+      itemIcon.setImageDrawable(getResources().getDrawable(android.R.drawable.ic_menu_agenda));
+      SubActionButton clothesButton = itemBuilder.setContentView(itemIcon).build();
+
+      mFloatMenu = new FloatingActionMenu.Builder(this)
+            .addSubActionView(addImageButton)
+            .addSubActionView(addCommentButton)
+            .addSubActionView(addRatingButton)
+            .addSubActionView(clothesButton)
+            .attachTo(mFab)
+            .build();
    }
 
    @Override
@@ -241,7 +306,7 @@ public class SenderoDetailWithImageActivity extends BaseActivity implements Obse
 
             @Override
             public void onAnimationStart(Animator animation) {
-
+               mFloatMenu.close(true);
             }
 
             @Override
@@ -304,5 +369,12 @@ public class SenderoDetailWithImageActivity extends BaseActivity implements Obse
          Utilities.getCameraPictureAndUpload(this);
       }
 
+   }
+
+   @Override
+   public void onScrollChanged() {
+      if (mFloatMenu != null) {
+         mFloatMenu.updateItemPositions();
+      }
    }
 }
